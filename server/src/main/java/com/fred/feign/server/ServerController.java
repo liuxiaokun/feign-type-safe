db98a0df5f81c356795bec782eca9d0c0309eebe
liuxiaokun <liuxk@cloudoer.com>
2019-04-04 15:59:20 +0800

    add server

---
+++ server/src/main/java/com/fred/feign/server/ServerController.java
@@ -0,0 +1,12 @@
+package com.fred.feign.server;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author liuxiaokun
+ * @version 0.0.1
+ * @since 2019/4/4
+ */
+@RestController
+public class ServerController {
+}
